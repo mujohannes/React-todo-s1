54185fe62c63d200c6ae1116d642011e77e208d9
Johannes Muljana <johannes.muljana@tafensw.edu.au>
2021-03-01 13:23:01 +1100

    updated app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+
 import './App.css';
+// list component
+function List( props ) {
+  const items = props.tasks.map( (item) => {
+    return <li>{item}</li>
+  } )
+
+  return(
+    <ul className="list">
+      {items}
+    </ul>
+  )
+}
 
 function App() {
+  // state to store tasks
+  const [tasks,setTasks] = useState( new Array() )
+
+  const SubmitHandler = (event) => {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    const task = data.get('input')
+    setTasks( tasks.concat(task) )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <form onSubmit={SubmitHandler} >
+          <input type="text" name="input" placeholder="add a task" />
+          <button type="submit">Add</button>
+        </form>
+        <List tasks={tasks} />
       </header>
     </div>
   );
